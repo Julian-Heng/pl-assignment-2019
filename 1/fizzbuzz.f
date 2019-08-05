96918484d0f5082fb705e688ca20312857e5d684
Julian Heng <19473701@student.curtin.edu.au>
2019-08-06 00:52:37 +0800

    Update fizzbuzz.f

--- 1/fizzbuzz.f
+++ 1/fizzbuzz.f
@@ -6,17 +6,16 @@
           finish = 100
 
           do while (i .LE. finish)
-              if (MOD(i, 3) .EQ. 0 .OR. MOD(i, 5) .EQ. 0) then
-                  if (MOD(i, 3) .EQ. 0) then
-                      write(*, "(A)", advance="no") "Fizz"
-                  endif
-                  if (MOD(i, 5) .EQ. 0) then
-                      write(*, "(A)", advance="no") "Buzz"
-                  endif
-                  write(*,"(A)") ""
-              else
-                  write(*,"(I0)") i
+              if (MOD(i, 3) .EQ. 0) then
+                  write(*, "(A)", advance="no") "Fizz"
               endif
+              if (MOD(i, 5) .EQ. 0) then
+                  write(*, "(A)", advance="no") "Buzz"
+              endif
+              if (MOD(i, 3) .NE. 0 .AND. MOD(i, 5) .NE. 0) then
+                  write(*,"(I0)", advance="no") i
+              endif
+              write(*,"(A)") ""
               i = i + 1
           enddo
 
