8f053259ca08a2ada70e5174ed82d4507238a866
Julian Heng <19473701@student.curtin.edu.au>
2019-08-21 22:28:34 +0800

    Use procedures

--- 3/bubble_sort.adb
+++ 3/bubble_sort.adb
@@ -9,37 +9,68 @@ procedure Bubble_Sort is
     use Ada.Strings;
     use Ada.Strings.Fixed;
 
-    Values  : array (0 .. Argument_Count - 1) of Integer;
-    Swapped : Boolean;
-    Temp    : Integer;
-begin
-    for I in Integer range 1 .. Argument_Count loop
-        begin
-            Values(I - 1) := Integer'Value(Argument(I));
-        exception
-            when CONSTRAINT_ERROR =>
-                Put_Line("Invalid Arguments: Needs to all be Integers");
-                Set_Exit_Status(1);
-                return;
-        end;
-    end loop;
+    type Integer_Array is array (0 .. Argument_Count - 1) of Integer;
 
-    loop
-        Swapped := False;
-
-        for I in Integer range 1 .. Values'Length - 1 loop
-            if Values(I - 1) > Values(I) then
-                Temp := Values(I);
-                Values(I) := Values(I - 1);
-                Values(I - 1) := Temp;
-                Swapped := True;
-            end if;
+    procedure Read_Command_Line_To_Integer_Array(Arr: out Integer_Array) is
+    begin
+        for I in Integer range 1 .. Argument_Count loop
+            begin
+                Arr(I - 1) := Integer'Value(Argument(I));
+            exception
+                when CONSTRAINT_ERROR =>
+                    Put_Line("Invalid Arguments: Needs to all be Integers");
+                    Set_Exit_Status(1);
+                    return;
+            end;
         end loop;
+    end Read_Command_Line_To_Integer_Array;
 
-        exit when not Swapped;
-    end loop;
 
-    for I in Values'Range loop
-        Put_Line(Trim(Values(I)'Image, Left));
-    end loop;
+    procedure Swap_Elements(
+        I, J : Integer;
+        Arr  : out Integer_Array)
+    is
+        Temp : Integer;
+    begin
+        Temp := Arr(I);
+        Arr(I) := Arr(J);
+        Arr(J) := Temp;
+    end Swap_Elements;
+
+
+    procedure Sort(Arr: out Integer_Array) is
+        Swapped : Boolean;
+    begin
+        loop
+            Swapped := False;
+            for I in Integer range 1 .. Arr'Length - 1 loop
+                if Arr(I - 1) > Arr(I) then
+                    Swap_Elements(I, I - 1, Arr);
+                    Swapped := True;
+                end if;
+            end loop;
+            exit when not Swapped;
+        end loop;
+    end Sort;
+
+
+    procedure Print_Integer_Array(Arr: in Integer_Array) is
+    begin
+        for I in Arr'Range loop
+            Put_Line(Trim(Arr(I)'Image, Left));
+        end loop;
+    end Print_Integer_Array;
+
+
+    procedure Main is
+        Arr  : Integer_Array;
+    begin
+        Read_Command_Line_To_Integer_Array(Arr);
+        Sort(Arr);
+        Print_Integer_Array(Arr);
+    end Main;
+
+
+begin
+    Main;
 end Bubble_Sort;
