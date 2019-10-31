94b00351878878523a0ff00b30ca7ca72076ef42
Julian Heng <19473701@student.curtin.edu.au>
2019-10-31 20:44:50 +0800

    Update assignment

--- 9/sort.scm
+++ 9/sort.scm
@@ -1,14 +1,15 @@
-(define (print-list l) (begin (display l) (display `"\n")))
-(define (random-list lim n)
+(define (pos-neg) (if (equal? (random 2) 1) `-1 `1))
+(define (rand-list lim n)
   (if (not (equal? n 0))
-    (cons (random lim) (random-list lim (- n 1)))
+    (cons (* (random lim) (pos-neg)) (rand-list lim (- n 1)))
     ()))
 
 
+(define (print-list l) (begin (display l) (display `"\n")))
 (define (list-length l)
   (if (not (null? l))
     (+ 1 (list-length (cdr l)))
-    (0)))
+    `0))
 
 
 (define (sorted? l)
@@ -18,21 +19,21 @@
     (else #f)))
 
 
-(define (cocktail-sort-forward l)
+(define (cocktail-forward l)
   (cond
     ((null? (cdr l)) l)
     (else
       (if (> (car l) (cadr l))
-        (cons (cadr l) (cocktail-sort-forward (cons (car l) (cddr l))))
-        (cons (car l) (cocktail-sort-forward (cdr l)))))))
+        (cons (cadr l) (cocktail-forward (cons (car l) (cddr l))))
+        (cons (car l) (cocktail-forward (cdr l)))))))
 
 
-(define (cocktail-sort-backward l)
+(define (cocktail-backward l)
   (cond
     ((null? (cdr l)) l)
     (else
       (let*
-        ((new_list (cocktail-sort-backward (cdr l)))
+        ((new_list (cocktail-backward (cdr l)))
          (_ new_list))
           (if (> (car l) (car new_list))
             (cons (car new_list) (cons (car l) (cdr new_list)))
@@ -41,12 +42,11 @@
 
 (define (cocktail-sort l)
   (cond
-    ((null? (cdr l)) l)
     ((sorted? l) l)
     (else
       (let*
-        ((forward (cocktail-sort-forward l))
-         (backward (cocktail-sort-backward forward)))
+        ((forward (cocktail-backward l))
+         (backward (cocktail-forward l)))
           (begin
             ; Uncomment to debug
             ; (display "Forward:  ") (print-list forward)
@@ -55,6 +55,6 @@
 
 
 ; Generate a list of 32 random numbers ranging from 0 to 100
-(print-list (cocktail-sort (random-list 100 32)))
+(print-list (cocktail-sort (rand-list 100 32)))
 
 ; vim: tabstop=2 shiftwidth=2 expandtab
