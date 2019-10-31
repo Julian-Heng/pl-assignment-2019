2220ec9b47c25e6114f280a1ab2a0872b7f47af9
Julian Heng <19473701@student.curtin.edu.au>
2019-10-31 20:16:23 +0800

    Update assignment

--- 4/test.sh
+++ 4/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 [[ -e "./build/list" && -x "./build/list" ]] && \
-    valgrind ./build/list < input.txt
+    valgrind ./build/list < input
---
+++ 9/test.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash
+
+[[ -e ./sort.scm ]] && \
+    mit-scheme --silent < ./sort.scm
